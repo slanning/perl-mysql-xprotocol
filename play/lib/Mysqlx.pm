7fb3e12c554c875b0528c8483ab262272a5386a9
Scott Lanning <lannings@gmail.com>
2016-05-26 13:32:09 +0200

    note on licence of Mysqlx.pm and removed .proto files
    I hadn't really thought about the license.
    Mysqlx.pm is generated from the .proto files
    so it's presumably under that license, GPL 2.
    Also removed the .proto files to simplify a bit.

--- play/lib/Mysqlx.pm
+++ play/lib/Mysqlx.pm
@@ -1,5 +1,11 @@
 package Mysqlx;
 
+# This is how Mysqlx.pm was generated:
+# The .proto files were from:
+# https://github.com/mysql/mysql-server/tree/5.7/rapid/plugin/x/protocol
+# protoc --perl-gpd_out=package=Mysqlx,pb_prefix=Mysqlx,prefix=Mysqlx:lib --proto_path=/home/slanning/xprotocol /home/slanning/xprotocol/*.proto
+# (Note: protoc is too dumb to deal with relative paths)
+
 use strict;
 use warnings;
 use MIME::Base64 qw();
--- proto/README.txt
+++
@@ -1,2 +0,0 @@
-These .proto files come from
-https://github.com/mysql/mysql-server/tree/5.7/rapid/plugin/x/protocol/
